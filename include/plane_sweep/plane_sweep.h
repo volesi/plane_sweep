dad8d973ad68a2fb3815d7b7e7eb1f4f10403866
Eikanger, Brage Gerdsønn <brage-gerdsonn.eikanger@ffi.no>
2018-05-04 17:08:36 +0200

    Added first draft of needed function declarations, and implemented ZNCC function.

--- include/plane_sweep/plane_sweep.h
+++ include/plane_sweep/plane_sweep.h
@@ -5,12 +5,51 @@
 #ifndef PLANE_SWEEP_PLANE_SWEEP_H
 #define PLANE_SWEEP_PLANE_SWEEP_H
 
+#include <opencv2/core.hpp>
+#include "dataset.h"
 
-class plane_sweep
+struct DepthPlane
+{
+  double depth;
+  double n_x;
+  double n_y;
+  double n_z;
+};
+
+struct DepthRange
+{
+  double min;
+  double max;
+  double interval;
+};
+
+class PlaneSweep
 {
 public:
+  PlaneSweep();
+
+  PlaneSweep(Dataset images);
+
+  void addImage(DataElement image);
+
+
+  cv::Mat getDepth(DepthPlane depth_plane);
+
+  void setDepthNormal(double n_x, double n_y, double n_z);
 
 private:
+
+  cv::Mat getHomography(DepthPlane plane);
+
+  cv::Mat mapTargetImgToRefImg(const cv::Mat& img, cv::Mat homography);
+
+  int computeSimilarity(const cv::Mat& img_ref, const cv::Mat& img_target);
+
+  cv::Mat computeFigureOfMerit(std::vector<int> similarity_measures);
+
+  int findBestFitDepth();
+
+  double ZNCC(const cv::Mat& img1, const cv::Mat& img2, const cv::Point2i& kernel_centre1, const cv::Point2i& kernel_centre2, int kernel_size);
 
 };
 
--- src/plane_sweep.cpp
+++ src/plane_sweep.cpp
@@ -3,3 +3,73 @@
 //
 
 #include "../include/plane_sweep/plane_sweep.h"
+
+namespace
+{
+double getAverage(const cv::Mat& img, const cv::Point2i& kernel_centre, int kernel_size);
+double getStdDeviation(const cv::Mat& img, const cv::Point2i& kernel_centre, int kernel_size);
+}
+
+
+double PlaneSweep::ZNCC(const cv::Mat& img1, const cv::Mat& img2, const cv::Point2i& kernel_centre1,
+                        const cv::Point2i& kernel_centre2, int kernel_size)
+{
+  const auto &u1 = kernel_centre1.x;
+  const auto &v1 = kernel_centre1.y;
+  const auto &u2 = kernel_centre2.x;
+  const auto &v2 = kernel_centre2.y;
+
+  const auto std_dev1 = getStdDeviation(img1, kernel_centre1, kernel_size);
+  const auto std_dev2 = getStdDeviation(img2, kernel_centre2, kernel_size);
+  const auto avg1 = getAverage(img1, kernel_centre1, kernel_size);
+  const auto avg2 = getAverage(img2, kernel_centre2, kernel_size);
+
+  double sum = 0;
+  for (int i = -kernel_size; i < kernel_size + 1; ++i)
+  {
+    for (int j = -kernel_size; j < kernel_size + 1; ++j)
+    {
+      sum += (img1.at<char>(u1+i, v1+j) - avg1)*(img2.at<char>(u2+i, v2+j) - avg2);
+    }
+  }
+
+  return sum / (std::pow(2*kernel_size + 1, 2) * std_dev1 * std_dev2);
+}
+
+namespace
+{
+double getAverage(const cv::Mat& img, const cv::Point2i& kernel_centre, int kernel_size)
+{
+  double sum = 0;
+  const auto &u = kernel_centre.x;
+  const auto &v = kernel_centre.y;
+
+  for (int i = -kernel_size; i < kernel_size + 1; ++i)
+  {
+    for (int j = -kernel_size; j < kernel_size + 1; ++j)
+    {
+      sum += img.at<char>(u+i , v+j);
+    }
+  }
+  return sum / std::pow(2*kernel_size + 1, 2);
+}
+
+double getStdDeviation(const cv::Mat& img, const cv::Point2i& kernel_centre, int kernel_size)
+{
+  double sum = 0;
+  const auto &u = kernel_centre.x;
+  const auto &v = kernel_centre.y;
+
+  const auto avg = getAverage(img, kernel_centre, kernel_size);
+
+  for (int i = -kernel_size; i < kernel_size + 1; ++i)
+  {
+    for (int j = -kernel_size; j < kernel_size + 1; ++j)
+    {
+      sum += std::pow(img.at<char>(u+i , v+j) - avg, 2);
+    }
+  }
+
+  return std::pow(sum, 0.5) / (2*kernel_size + 1);
+}
+}
